48f7864561ab8a30143507580d784cedfb0d93f7
Bombardier256 <4477617@gmail.com>
2025-04-30 17:34:53 +0300

    Correct flakes6

--- cinema/views.py
+++ cinema/views.py
@@ -120,9 +120,9 @@ class OrderViewSet(viewsets.ModelViewSet):
         if self.action == "list":
             return (
                 self.queryset.filter(user=self.request.user).prefetch_related(
-                "tickets__movie_session",
-                "tickets__movie_session__cinema_hall",
-                "tickets__movie_session__movie")
+                    "tickets__movie_session",
+                    "tickets__movie_session__cinema_hall",
+                    "tickets__movie_session__movie")
             )
         elif self.action == "retrieve":
             return (self.queryset.filter(user=self.request.user).
