9273eed46e5dbfae275733428ee930e37b1ff0c6
Bombardier256 <4477617@gmail.com>
2025-04-30 17:27:57 +0300

    Correct flakes2

--- cinema/views.py
+++ cinema/views.py
@@ -119,16 +119,14 @@ class OrderViewSet(viewsets.ModelViewSet):
         queryset = self.queryset
         if self.action == "list":
             return (self.queryset.filter(user=self.request.user)
-                .prefetch_related(
-                    "tickets__movie_session",
-                    "tickets__movie_session__cinema_hall",
-                    "tickets__movie_session__movie")
-            )
+            .prefetch_related(
+                "tickets__movie_session",
+                "tickets__movie_session__cinema_hall",
+                "tickets__movie_session__movie"))
         elif self.action == "retrieve":
-            return (self.queryset.filter(user=self.request.user)
-                .prefetch_related(
-                    "tickets__movie_session")
-            )
+            return (self.queryset.filter(user=self.request.user).
+                    prefetch_related("tickets__movie_session"))
+
         return queryset
 
     def get_serializer_class(self):
